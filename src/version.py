ea21a18ba91d578bfdde70025db55aeaa9e218ea
Dirk Meyer <dischi@freevo.org>
2005-07-07 12:22:35 +0000

    remove version file
    git-svn-id: svn://svn.freevo.org/kaa/trunk/metadata@455 a8f5125c-1e01-0410-8897-facf34644b8e

--- src/version.py
+++
@@ -1,9 +0,0 @@
-# the intention of this file is to make it possible to check
-# for the program using this lib if it's an up-to-date version
-
-# "offical" version of kaa-metadata
-VERSION = '0.5.0.cvs'
-
-# latest major change (date as integer)
-CHANGED = 20040629
-
