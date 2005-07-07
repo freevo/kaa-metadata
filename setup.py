92610b9bca1caabc27dc4b7fb52d0f3f722f2622
Dirk Meyer <dischi@freevo.org>
2005-07-07 16:45:43 +0000

    better libdvdread detection
    git-svn-id: svn://svn.freevo.org/kaa/trunk/metadata@471 a8f5125c-1e01-0410-8897-facf34644b8e

--- setup.py
+++ setup.py
@@ -31,8 +31,6 @@
 
 # python imports
 import sys
-import popen2
-import os
 
 try:
     # kaa base imports
@@ -40,33 +38,32 @@ try:
 except ImportError:
     print 'kaa.base not installed'
     sys.exit(1)
-    
-extensions = [ Extension('src/disc/cdrom', ['src/disc/cdrommodule.c']) ]
 
-# check for libdvdread (bad hack!)
-# Windows does not have Popen4, so catch exception here
+# cdrom extension, FIXME: check if it will compile
+cdrom = Extension('src/disc/cdrom', ['src/disc/cdrommodule.c'])
+
+# check for libdvdread
+ifoparser = Extension('kaa/metadata/disc/ifoparser', ['src/disc/ifomodule.c'],
+                      libraries=[ 'dvdread' ])
+
 try:
-    child = popen2.Popen4('gcc -ldvdread')
-    if child.fromchild.readline().find('cannot find') == -1:
-        # gcc failed, but not with 'cannot find', so libdvd must be
-        # somewhere (I hope)
-        extensions.append(Extension('kaa/metadata/disc/ifoparser',
-                                    ['src/disc/ifomodule.c'],
-                                    libraries=[ 'dvdread' ],
-                                    library_dirs=['/usr/local/lib'],
-                                    include_dirs=['/usr/local/include']))
-    child.wait()
-except AttributeError, e:
-    print "No Popen4 found. This seems to be Windows."
-    print "Installing without libdvdread support."
-    # Hack: disable extensions for Windows.
-    # This would better be done by a clean detect of windows. But how?
-    extensions = []
+    if not ifoparser.check_cc([], '', '-ldvdread'):
+        print 'Warning: libdvdread is missing.'
+        raise AttributeError()
 
+    if not ifoparser.check_cc(['<dvdread/dvd_reader.h>'], '', '-ldvdread'):
+        print 'Warning: libdvdread header file is missing.'
+        raise AttributeError()
+
+    ext_modules = [ cdrom, ifoparser ]
+except AttributeError:
+    print 'The DVD parser will be disabled'
+    ext_modules = []
+    
 setup (module      = 'metadata',
        version     = '0.4.99.1',
        description = "Module for retrieving information about media files",
        author      = "Thomas Schueppel, Dirk Meyer",
        scripts     = [ 'bin/mminfo' ],
-       ext_modules = extensions
+       ext_modules = ext_modules
       )
