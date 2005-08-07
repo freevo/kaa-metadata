23ffa84f0b26cf88b8ddd8f4adbf9a6d6b0d0e36
Dirk Meyer <dischi@freevo.org>
2005-08-07 09:12:03 +0000

    fix install location of the cdrom module
    git-svn-id: svn://svn.freevo.org/kaa/trunk/metadata@639 a8f5125c-1e01-0410-8897-facf34644b8e

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ except ImportError:
     sys.exit(1)
 
 # cdrom extension, FIXME: check if it will compile
-cdrom = Extension('src/disc/cdrom', ['src/disc/cdrommodule.c'])
+cdrom = Extension('kaa/metadata/disc/cdrom', ['src/disc/cdrommodule.c'])
 
 # check for libdvdread
 ifoparser = Extension('kaa/metadata/disc/ifoparser', ['src/disc/ifomodule.c'],
