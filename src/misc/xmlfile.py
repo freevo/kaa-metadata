ac11c5927a6cc34f7264bb344dc1095573281896
Dirk Meyer <dischi@freevo.org>
2007-03-19 10:16:58 +0000

    use sax parser for xmlfile
    git-svn-id: svn://svn.freevo.org/kaa/trunk/metadata@2574 a8f5125c-1e01-0410-8897-facf34644b8e

--- src/misc/xmlfile.py
+++ src/misc/xmlfile.py
@@ -32,7 +32,7 @@
 # python imports
 import os
 import logging
-import libxml2
+import xml.sax
 
 # kaa.metadata imports
 import kaa.metadata.core as core
@@ -43,8 +43,12 @@ log = logging.getLogger('metadata')
 
 XML_TAG_INFO = {
     'image':  'Bins Image Description',
+    'album':  'Bins Album Description',
     'freevo': 'Freevo XML Definition'
     }
+
+class Identified:
+    pass
 
 class XML(core.Media):
 
@@ -64,25 +68,23 @@ class XML(core.Media):
             self.type  = 'HTML Document'
             return
 
-        ctxt = libxml2.createFileParserCtxt(file.name)
-        # Silence parse errors
-        ctxt.setErrorHandler(lambda *args: None, None)
-        ctxt.parseDocument()
-
+        handler = xml.sax.ContentHandler()
+        handler.startElement = self.startElement
+        parser = xml.sax.make_parser()
+        parser.setContentHandler(handler)
         try:
-            doc = ctxt.doc()
-        except libxml2.parserError:
-            raise core.ParseError()
+            parser.parse(file)
+        except (KeyboardInterrupt, SystemExit):
+            sys.exit(0)
+        except Identified:
+            pass
+    
 
-        if not doc or not doc.children or not doc.children.name:
-            raise core.ParseError()
-
-        tag = doc.children.name
-        if tag in XML_TAG_INFO:
-            self.type = XML_TAG_INFO[tag]
+    def startElement(self, name, attr):
+        if name in XML_TAG_INFO:
+            self.type = XML_TAG_INFO[name]
         else:
             self.type = 'XML file'
-        doc.freeDoc()
-
-
+        raise Identified
+        
 factory.register( 'text/xml', ('xml', 'fxd', 'html', 'htm'), XML )
