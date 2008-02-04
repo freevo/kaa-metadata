ac7203b918c7cdb603e7b149f23079d63a413c8d
Dirk Meyer <dischi@freevo.org>
2008-02-04 18:30:10 +0000

    release kaa.metadata 0.7.2
    git-svn-id: svn://svn.freevo.org/kaa/trunk/metadata@3025 a8f5125c-1e01-0410-8897-facf34644b8e

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ if not cdrom.has_python_h():
     ext_modules = [ ]
     
 setup (module      = 'metadata',
-       version     = '0.7.1',
+       version     = '0.7.2',
        license     = 'GPL',
        summary     = 'Module for retrieving information about media files',
        author      = "Thomas Schueppel, Dirk Meyer",
