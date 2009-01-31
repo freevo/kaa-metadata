133c1d24ef29ad0021f7f64c1469660e918bdff6
Dirk Meyer <dischi@freevo.org>
2009-01-31 19:31:22 +0000

    add timestamp and orientation parsing
    git-svn-id: svn://svn.freevo.org/kaa/trunk/metadata@3826 a8f5125c-1e01-0410-8897-facf34644b8e

--- src/image/generic.py
+++ src/image/generic.py
@@ -10,8 +10,8 @@
 # while an aplication like Freevo does not.
 #
 # -----------------------------------------------------------------------------
-# kaa-Metadata - Media Metadata for Python Copyright (C) 2009 Thomas
-# Schueppel, Dirk Meyer
+# kaa-Metadata - Media Metadata for Python
+# Copyright (C) 2009 Dirk Meyer
 #
 # First Edition: Dirk Meyer <dischi@freevo.org>
 # Maintainer:    Dirk Meyer <dischi@freevo.org>
@@ -36,26 +36,41 @@
 
 __all__ = ['Parser']
 
+# python imports
+import time
+
 # import kaa.metadata.image core
 import core
 import exiv2
 
-# TODO: improve the mapping
-# use 'mminfo -d 2 filename' to get a list of detected attributes
 mapping = {
-    # generic mapping
+    # Generic mapping
     'Image.Width': 'width',
     'Image.Height': 'height',
     'Image.Mimetype': 'mime',
     'Image.Thumbnail': 'thumbnail',
     'Image.Keywords': 'keywords',
-    # exif mapping
+
+    # EXIF mapping
     'Exif.Image.Model': 'hardware',
     'Exif.Image.Software': 'software',
     'Exif.Canon.OwnerName': 'artist',
-    # iptc mapping
-    'Iptc.Application2.Byline': 'artist'
+
+    # IPTC mapping
+    'Iptc.Application2.Byline': 'artist',
+    'Iptc.Application2.BylineTitle': 'title',
+    'Iptc.Application2.Headline': 'title',
+    'Iptc.Application2.Writer': 'author',
+    'Iptc.Application2.Credit': 'author',
+    'Iptc.Application2.Byline': 'author',
+    'Iptc.Application2.LocationName': 'country',
+    'Iptc.Application2.Caption': 'description',
+    'Iptc.Application2.City': 'city',
+    'Iptc.Application2.SubLocation': 'location'
 }
+
+# use 'mminfo -d 2 filename' to get a list of detected attributes if
+# you want to improve this list
 
 class Generic(core.Image):
 
@@ -67,6 +82,29 @@ class Generic(core.Image):
         # The exiv2 parser just dumps everything it sees in a dict.
         # The mapping from above is used to convert the exiv2 keys to
         # kaa.metadata keys.
-        self._appendtable('exiv2', exiv2.parse(file.name))
+        metadata = exiv2.parse(file.name)
+        self._appendtable('exiv2', metadata)
+
+        # parse timestamp
+        t = metadata.get('Exif.Photo.DateTimeOriginal')
+        if not t:
+            # try the normal timestamp which may be last-modified
+            t = metadata.get('Exif.Image.DateTime')
+        if t:
+            try:
+                t = time.strptime(str(t), '%Y:%m:%d %H:%M:%S')
+                self.timestamp = int(time.mktime(t))
+            except ValueError:
+                # Malformed time string.
+                pass
+
+        # parse orientation
+        orientation = metadata.get('Exif.Image.Orientation')
+        if orientation == 2:
+            self.rotation = 180
+        if orientation == 5:
+            self.rotation = 270
+        if orientation == 6:
+            self.rotation = 90
 
 Parser = Generic
